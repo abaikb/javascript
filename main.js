bb26f8c327e352f11129c0c8ee294628e7cd71de
abaikb <abaikb@icloud.com>
2023-03-02 11:54:17 +0600

    true faulse

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-let year = prompt('year of brith')
+/*let year = prompt('year of brith')
 let age = 2023 - Number(year)
 console.log('ваш', age ,'!')
 
 let a = 5
 console.log(a)
 a = 10
-console.log(a)
+console.log(a)*/
+
+console.log(5 > 10)
+console.log(5 === 5)
+console.log(10 - '5')
+console.log(10 - true)
+console.log(Boolean(0))
+//Boolean только 0 будет  foulse
+console.log(10 - Number(true))
+
+
