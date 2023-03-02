f360e0e7ebda2b7817e121651b5d2270606124ec
abaikb <abaikb@icloud.com>
2023-03-02 13:18:34 +0600

    class-work

--- main.js
+++ main.js
@@ -15,4 +15,29 @@ console.log(Boolean(0))
 //Boolean только 0 будет  foulse
 console.log(10 - Number(true))
 
+//Первая задание 1
+let num1 = prompt("Введите первое число:");
+let num2 = prompt("Введите второе число:");
+
+console.log(Number(num1) * Number(num2));
+
+//Вторая задача 2
+let december = 31;
+let january = 31;
+let february = 29;
+
+console.log("Количиства дней зимой", (december + january + february));
+
+//Третая задача 3
+let hours8 = prompt("Введите вашу почасовую ставку:");
+let weeks5 = prompt("Введите количество недель, которые вы проработали:");
+
+let hours = Number(hours8);
+let days = Number(weeks5);
+
+
+console.log("Ваша заработная плата", (hours * 8) * (days * 5));
+
+
+
 
